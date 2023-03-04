c9131786c7bf9c13b25e7308b16f06c8dfc2f65e
sjohnston82 <72237516+sjohnston82@users.noreply.github.com>
2023-03-04 15:31:30 -0500

    filling out db models

--- src/pages/first-login/index.tsx
+++ src/pages/first-login/index.tsx
@@ -1,14 +1,17 @@
-import React from 'react'
-import Header from '~/components/layout/Header'
+import React from "react";
+import Header from "~/components/layout/Header";
 
 const FirstLogin = () => {
   return (
     <div>
       <Header />
       <h1 className="text-center">Thank you for choosing QuarterMaster!</h1>
-
+      <div className="">
+        Create a household to get started!
+        <button className="btn btn-primary">Create Household</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default FirstLogin
+export default FirstLogin;
---
+++ src/server/api/routers/household.ts
@@ -0,0 +1,11 @@
+import { z } from "zod";
+
+import {
+  createTRPCRouter,
+  publicProcedure,
+  protectedProcedure,
+} from "~/server/api/trpc";
+
+export const householdRouter = createTRPCRouter({
+  create: protectedProcedure.
+});
